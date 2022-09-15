3afbcc6763329b58fd692e95a507787b2f5f70a3
Zafrul Hasan Khan (Nasim) <nasimkhan18003@gmail.com>
2022-09-15 18:58:11 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Basic from './Basic';
+function App() {
+  return (
+    <div className="App">
+     <Basic />
+      
+    </div>
+    
+  );
+}
+
+export default App;
